4c9e0d24aa28fe679d0398becb3b6268d2af26e7
Nicolas <nicolas@uucidl.com>
2018-07-25 08:34:45 +0200

    Support manual installation

--- deps/fetch.sh
+++ deps/fetch.sh
@@ -2,9 +2,16 @@
 set -e
 depfile="${1:?missing depfile}"
 depname="$(basename "${depfile}" .txt)"
+
+is_manual="$(sed -n -e 's/@manual *$/is_manual/p' "${depfile}")"
 url="$(sed -n -e 's/@url: *//p' "${depfile}")"
 name="$(sed -n -e 's/@name: *//p' "${depfile}")"
 if [[ -z $"{name}" ]] ; then name="$(basename "${depfile}" .txt)" ; fi
+if [[ "${is_manual}" == "is_manual" ]]; then
+	curl -L "${url}" -O
+	printf "WARN: Please extract this dependency manually.\n"
+	exit 0
+fi
 package="${depname}.zip"
 curl -L "${url}" -o "${package}"
 depdir="./"${depname}""
