4b4bdd36ae30abf80d21df9766e843cd2c6bf3dc
Nicolas <nicolas@uucidl.com>
2018-08-01 20:21:54 +0200

    Let us compile and run kaon_scaffold on Macos

--- build_osx.sh
+++ build_osx.sh
@@ -1,9 +1,23 @@
 set -e
 [[ -d "output" ]] || mkdir -p "output"
 export IONHOME=$(pwd)/deps/bitwise/ion
-output/ion -os osx -arch x64 -o output/docking_demo_program.c \
-  docking_demo_program
-cc -o output/docking_demo.elf output/docking_demo_program.c \
-  -F./deps/SDL2_osx -framework SDL2 \
-  -I./deps/SDL2_osx/SDL2.framework/Headers/ 
-printf "PROGRAM\t%s\n" "$(pwd)"/output/docking_demo.elf
+function build_program_module() {
+	Module=$1
+	Name=$2
+	pushd src
+	../output/ion -os osx -arch x64 -o ../output/"${Module}".c \
+  	"$Module"
+  	popd
+	cc -o output/"${Name}".elf output/"${Module}".c \
+  		-F./deps/SDL2_osx -framework SDL2 \
+  		-I./deps/SDL2_osx/SDL2.framework/Headers/ \
+  		-I./deps/nanovg/src \
+  		-I./deps/GL3/include \
+  		-I./
+	printf "PROGRAM\t%s\n" "$(pwd)"/output/"${Name}".elf	
+}
+
+build_program_module docking_demo_program docking_demo
+build_program_module kaon_scaffold_program kaon_scaffold
+
+ 
